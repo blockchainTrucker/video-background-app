85209a1340596f9450dd652e717acdd3b556832b
Jesse Wachtel <84250138+blockchainTrucker@users.noreply.github.com>
2024-06-23 13:58:25 -0400

    initial push

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import videoSrc from './assets/video.mp4'; // Ensure you have a video file in th
 function App() {
   return (
     <div className="App">
-      <VideoBackground videoSrc={videoSrc}>
+      <VideoBackground videoSrc={videoSrc} >
         <h1>Welcome to My Website</h1>
         <p>Enhance your web experience with video backgrounds</p>
       </VideoBackground>
